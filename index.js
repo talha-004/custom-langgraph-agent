816f7a0dca666787c14ebe3c7e5fcd610e0b5d06
Talha-2023 <syedtalhaahmed123786@gmail.com>
2026-04-21 07:20:29 +0530

    feat: add TavilySearch tool and integrate into workflow. Help to do webseach

--- index.js
+++ index.js
@@ -1,25 +1,51 @@
 import { ChatGroq } from "@langchain/groq";
 import { MessagesAnnotation, StateGraph } from "@langchain/langgraph";
+import { ToolNode } from "@langchain/langgraph/prebuilt";
+import { TavilySearch } from "@langchain/tavily";
 import { log } from "node:console";
 import readline from "node:readline/promises";
 
+// agent tools
+const tool = new TavilySearch({
+  maxResults: 3,
+  topic: "general",
+});
+const tools = [tool];
+const toolNode = new ToolNode(tools);
+
+// modle
 const llm = new ChatGroq({
   model: "openai/gpt-oss-120b",
   temperature: 0,
   maxRetries: 2,
-});
+}).bindTools(tools);
 
+// agent function
 async function callModel(state) {
   console.log("calling llm");
   const response = await llm.invoke(state.messages);
   return { messages: [response] };
 }
 
+// control flow
+function shouldContinue(state) {
+  const lastMessage = state.messages[state.messages.length - 1];
+  if (lastMessage.tool_calls.length > 0) {
+    console.log("Searching the web...");
+    return "tools";
+  }
+  return "__end__";
+}
+
+// workflow
 const workflow = new StateGraph(MessagesAnnotation)
   .addNode("agent", callModel)
+  .addNode("tools", toolNode)
   .addEdge("__start__", "agent")
-  .addEdge("agent", "__end__");
+  .addEdge("tools", "agent")
+  .addConditionalEdges("agent", shouldContinue);
 
+// compile the workflow
 const app = workflow.compile();
 
 async function main() {
